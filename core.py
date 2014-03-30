d8305d586aec4c999c0ad332b8d518b16f4c9873
Damien Lesgourgues <damien@lesgourgues.fr>
2014-03-30 15:18:45 +0200

    change print by prtErr and debug calls

--- core.py
+++ core.py
@@ -22,29 +22,29 @@ config = configparser.ConfigParser()
 try:
     config.readfp(open(configFile))
 except FileNotFoundError:
-    print("Configuration file {0} doesn't exist.".format(configFile))
+    prtErr("Configuration file {0} doesn't exist.".format(configFile))
     sys.exit()
 except PermissionError:
-    print("Unable to read configuration file. Please check permissions for {0}.".format(configFile))
+    prtErr("Unable to read configuration file. Please check permissions for {0}.".format(configFile))
     sys.exit()
 except:
-    print("Unable to read configuration or configuration in wrong format.")
-    print("Please report following error to https://github.com/dlgg/Ouroboros/issues")
-    print(sys.exc_info())
+    prtErr("Unable to read configuration or configuration in wrong format.")
+    prtErr("Please report following error to https://github.com/dlgg/Ouroboros/issues")
+    prtErr(sys.exc_info())
     sys.exit()
 
 cfgSect = config.sections()
-print("Getting lists of sections : {0}".format(cfgSect))
-print()
-print("For each sections print options configured and their values.")
+debug("Getting lists of sections : {0}".format(cfgSect))
+debug("")
+debug("For each sections print options configured and their values.")
 for section in cfgSect:
-    print("{0} :".format(section))
+    debug("{0} :".format(section))
     for option in config.options(section):
-        print("{0} -> {1}".format(option, config.get(section, option)))
-    print()
+        debug("{0} -> {1}".format(option, config.get(section, option)))
+    debug("")
 
-print("Starting IRC initialisation for server {0} ({1}:{2}) and identification {3}!{4}@host:\"{5}\" for chans {6}".format(config['irc1']['name'],config['irc1']['host'],config['irc1']['port'],config['irc1']['nick'],config['irc1']['ident'],config['irc1']['realname'],','.join(config['irc1']['chans'].split())))
-print()
+debug("Starting IRC initialisation for server {0} ({1}:{2}) and identification {3}!{4}@host:\"{5}\" for chans {6}".format(config['irc1']['name'],config['irc1']['host'],config['irc1']['port'],config['irc1']['nick'],config['irc1']['ident'],config['irc1']['realname'],','.join(config['irc1']['chans'].split())))
+debug("")
 
 i = [None,None,None,None,None,None]
 i[0] = irc.Irc(config['ouroboros'], config['irc1'])
