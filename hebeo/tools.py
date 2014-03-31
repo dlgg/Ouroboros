9fa67e26c1a8afefd40bc293750f3a61b3e98c67
Damien Lesgourgues <damien@lesgourgues.fr>
2014-03-31 02:25:18 +0200

    strip leading and ending whitespace/CRLF

--- hebeo/tools.py
+++ hebeo/tools.py
@@ -5,8 +5,8 @@
 from colorama import Fore
 
 def debug(msg):
-    print(Fore.YELLOW + "DEBUG >>> {0}".format(msg))
+    print(Fore.YELLOW + "DEBUG >>> {0}".format(msg.strip()))
 
 def prtErr(msg):
-    print(Fore.RED + "ERROR >>> {0}".format(msg))
+    print(Fore.RED + "ERROR >>> {0}".format(msg.strip()))
 
