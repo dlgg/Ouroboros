365208ac89cf803f2612804c77378287837c47f4
Damien Lesgourgues <damien@lesgourgues.fr>
2016-04-24 06:57:15 +0200

    simplify time and add channel name

--- hebeo/youtube.py
+++ hebeo/youtube.py
@@ -25,10 +25,36 @@ def ytVideoStats(videoid):
     ytData=json.loads(ytResp.decode("UTF-8"))
     stats = {}
     stats['link']     = "https://youtu.be/{}" + videoid
+    stats['user']     = ytData['items'][0]['snippet']['channelTitle']
     stats['title']    = ytData['items'][0]['snippet']['title']
-    stats['duration'] = ytData['items'][0]['contentDetails']['duration']
+    stats['duration'] = ytTime2Sec(ytData['items'][0]['contentDetails']['duration'])
     stats['view']     = ytData['items'][0]['statistics']['viewCount']
     stats['like']     = ytData['items'][0]['statistics']['likeCount']
     stats['dislike']  = ytData['items'][0]['statistics']['dislikeCount']
     return stats
+
+def ytTime2Sec(time):
+    seconds=0
+    s=0
+    for t in time[2:]:
+        if t.isdigit():
+            if s == 0:
+                s=int(t)
+            else:
+                s=s*10
+                s+=int(t)
+        else:
+            if t == "H":
+                seconds+=s*3600
+                s=0
+            elif t == "M":
+                seconds+=s*60
+                s=0
+            elif t == "S":
+                seconds+=s
+                s=0
+            else:
+                s=0
+    return seconds
+            
 
